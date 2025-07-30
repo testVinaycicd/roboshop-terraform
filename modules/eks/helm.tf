565e74a78f6f6dbdba0d7340da599c0a5d8218b3
vinay <vinaybkrishna10@gmail.com>
2025-07-30 16:29:16 +0530

    new testing

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -41,3 +41,12 @@ resource "helm_release" "kube-prometheus-stack" {
 
 
 }
+
+resource "helm_release" "ingress" {
+  depends_on = [null_resource.kubeconfig,helm_release.kube-prometheus-stack]
+  name       = "ingress"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+
+
+}
