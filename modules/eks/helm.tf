74f6d44c44518020b2acf1c79ae99a426a95a6dc
vinay <vinaybkrishna10@gmail.com>
2025-07-30 15:52:59 +0530

    new testing

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -32,3 +32,12 @@ kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}
 EOF
   }
 }
+
+resource "helm_release" "kube-prometheus-stack" {
+  depends_on = [null_resource.kubeconfig]
+  name       = "kube-prom-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+
+
+}
