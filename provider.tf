f864e47c78b8a5ca89586be611886d07ae25ec3e
vinay <vinaybkrishna10@gmail.com>
2025-07-30 15:55:23 +0530

    new testing

--- provider.tf
+++ provider.tf
@@ -6,3 +6,9 @@ provider "vault" {
   address = "http://vault.mikeydevops1.online:8200/"
   token = var.vault_token
 }
+
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
