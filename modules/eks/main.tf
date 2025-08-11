17dddbf4e1d53aab019016c1524050fbc3575dcd
vinay <vinaybkrishna10@gmail.com>
2025-08-11 22:23:17 +0530

    new testing

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -39,15 +39,15 @@ resource "aws_eks_node_group" "main" {
 
 }
 
-data "aws_eks_cluster_auth" "main" {
-  name = aws_eks_cluster.main.name
-}
-
-provider "kubernetes" {
-  host                   = aws_eks_cluster.main.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.main.token
-}
+# data "aws_eks_cluster_auth" "main" {
+#   name = aws_eks_cluster.main.name
+# }
+#
+# provider "kubernetes" {
+#   host                   = aws_eks_cluster.main.endpoint
+#   cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.main.token
+# }
 
 # resource "kubernetes_config_map_v1" "aws_auth" {
 #   metadata {
